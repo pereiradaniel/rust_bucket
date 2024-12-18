7e22be42e4cc7ca3c16e6a45275291cdec5a3721
Daniel <13129650+pereiradaniel@users.noreply.github.com>
2024-12-18 15:42:16 -0500

    Added comments to label loops in array_loop.rs

--- control_flow/array_loop.rs
+++ control_flow/array_loop.rs
@@ -1,4 +1,6 @@
 fn main() {
+
+    // Loops through an array using while:
     let a = [10, 20, 30, 40, 50];
     let mut index = 0;
 
@@ -8,12 +10,13 @@ fn main() {
         index += 1;
     }
 
+    // Loops through an array using for (element):
     let b = [15, 25, 35, 45, 55];
-
     for element in b {
         println!("the value is: {element}");
     }
 
+    // Loops through a Range of numbers in descending order:
     for number in (1..4).rev() {
         println!("{number}!");
     }
